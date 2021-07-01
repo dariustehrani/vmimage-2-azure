d376b336d820e62d45144012467101ae4633239f
Darius Tehrani <darius.tehrani@microsoft.com>
2021-07-02 00:46:04 +0200

    backup.

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # vmimage-2-azure
 converts ova and vmdk VM Images for the use with Azure.
 
-# diagram
+## diagram
 ![alt text](/azure-image-converter.png)
 
 
-#  related links
+##  related links
 
 https://docs.openstack.org/de/image-guide/convert-images.html  
 https://www.vmware.com/support/developer/ovf/  
 http://www.hurryupandwait.io/blog/creating-a-hyper-v-vagrant-box-from-a-virtualbox-vmdk-or-vdi-image  
 
-image cleansing
-https://docs.microsoft.com/de-de/azure/virtual-machines/windows/prepare-for-upload-vhd-image  
+### image cleansing
+https://docs.microsoft.com/en-us/azure/virtual-machines/linux/create-upload-generic
+https://docs.microsoft.com/de-de/azure/virtual-machines/windows/prepare-for-upload-vhd-image 
+
+### Azure VHD utilities.
+### This project provides a Go package to read Virtual Hard Disk (VHD) file, a CLI interface to upload local VHD to Azure storage and to inspect a local VHD.
+https://github.com/microsoft/azure-vhd-utils
--- convertervm.sh
+++ convertervm.sh
@@ -2,7 +2,7 @@
 
 # fundamentals
 sudo apt-get update && sudo apt-get dist-upgrade -y 
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl ca-certificates apt-transport-https lsb-release
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl ca-certificates apt-transport-https lsb-release progress
 
 # docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -20,13 +20,13 @@ sudo usermod -aG libvirt $USER
 sudo usermod -aG kvm $USER
 
 # install packer & terraform
-$ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-$ sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-$ sudo apt-get update && sudo apt-get install -y packer terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install -y packer terraform
 
 # terraform autocomplete
-$ touch ~/.bashrc
-$ terraform -install-autocomplete
+touch ~/.bashrc
+terraform -install-autocomplete
 
 # azcli
 AZ_REPO=$(lsb_release -cs)
@@ -34,10 +34,37 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO 
 sudo apt-get update
 sudo apt-get install azure-cli blobfuse azcopy
 
+# azcopy
+curl -s -D- https://aka.ms/downloadazcopy-v10-linux | grep ^Location
+wget -O azcopy_v10.tar.gz https://aka.ms/downloadazcopy-v10-linux && tar -xf azcopy_v10.tar.gz --strip-components=1
+sudo chown root:root azcopy
+sudo mv azcopy /usr/local/bin/
 
-# blobfuse
-# wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
-# sudo dpkg -i packages-microsoft-prod.deb
-# sudo apt-get update
-# sudo apt-get install blobfuse
 
+# managed identity access  storage access
+#https://techcommunity.microsoft.com/t5/azure-paas-blog/mount-blob-storage-on-linux-vm-using-managed-identities-or/ba-p/1821744
+
+# blobfuse usage
+sudo mkdir /mnt/resource/blobfusetmp -p
+sudo chown $USER /mnt/resource/blobfusetmp
+touch ~/fuse_connection.cfg
+chmod 600 ~/fuse_connection.cfg
+
+# fuse connection
+accountName 4711vmstorage
+authType MSI
+containerName data
+
+# create storage account 
+# todo
+assign "storage blob contributor" role to VM managed identity
+
+mkdir /mnt/img-data
+sudo blobfuse /mnt/img-data --tmp-path=/mnt/resource/blobfusetmp  --config-file=/home/darius/fuse_connection.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120
+
+# run qemu
+cd /mnt/img-data
+qemu-img convert -f vmdk -O vpc haproxy.ova.vmdk linux-haproxy.vhd & progress -mp $!
+
+## unmount command for blobfuse
+## fusermount -u /mnt/img-data
