b5cab7eb3a563c756c13b68feb4cfdc8d76815e1
Daniel Falkner <dafalkne@microsoft.com>
2021-07-13 12:56:34 +0200

    Image Converter VM

---
+++ arm/Bootstrap.ps1
@@ -0,0 +1,19 @@
+#Force TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Assign Packages to Install
+$Packages = '7zip',`
+            'wget',
+	        'azcopy10'
+
+#Install Packages
+ForEach ($PackageName in $Packages)
+{choco install $PackageName -y}
+
+D: 
+wget https://www.starwindsoftware.com/tmplink/starwindconverter.exe
+starwindconverter.exe /silent
